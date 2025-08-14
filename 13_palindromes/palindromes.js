6057dfb0a1493ad55a8d8e8683e2b1838917372b
Arjay Dimaunahan <arjay213112@gmail.com>
2025-08-14 18:25:06 +0800

    Complete findTheOldest exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+            const alphanumerical = `abcdefghijklmnopqrstuvwxyz1234567890`;
+            
+            const cleansedString = string
+            .toLowerCase()
+            .split('')
+            .filter(character => alphanumerical.includes(character))
+            .join('');
 
-};
+            const reverseString = cleansedString.split('').reverse().join('');
 
+            return reverseString === cleansedString;
+        };
 // Do not edit below this line
 module.exports = palindromes;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(birth, death){
+    if(!death){
+        return new Date().getFullYear - birth;
+    }else{
+        return death - birth;
+    }
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? oldestPerson : currentPerson;
+    })
 };
 
 // Do not edit below this line
